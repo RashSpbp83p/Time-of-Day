03ea7cea48a5825cd963e736bd75980c2d432b9d
RashSpbp83p <127841971+RashSpbp83p@users.noreply.github.com>
2023-06-14 11:50:23 +0300

    Add files via upload

---
+++ TimeOfDay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PrismaBoy
+{
+    [Serializable]
+    
+    public class TimeOfDay
+    {
+        public int Minutes;
+        public int Hours;
+
+        public TimeOfDay(int hours, int minutes)
+        {
+            Minutes = minutes;
+            Hours = hours;
+        }
+    }
+}
